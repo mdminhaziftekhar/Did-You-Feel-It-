528d898cebf8dd8b5ef0d22db963107ed9b7021b
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2021-12-01 23:18:46 +0600

    Handled the exceptions those might comeup in future

--- app/src/main/java/com/example/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/didyoufeelit/MainActivity.java
@@ -40,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Event doInBackground(String... urls) {
 
+            //Don't perform the request if there are no URL's or the first URL is null
+            if(urls.length < 1 || urls[0]==null) return null;
+
             // Perform the HTTP request for earthquake data and process the response.
             Event result = Utils.fetchEarthquakeData(urls[0]);
 
@@ -48,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Event result) {
+            //If there is no result, do nothing
+            if(result == null) return;
+            
             // Update the information displayed to the user.
             updateUi(result);
         }
